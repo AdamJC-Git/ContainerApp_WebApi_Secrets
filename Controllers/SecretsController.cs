0db1dd4b299a57a970ba1e49e4baec399b020db0
AdamJC-Git <52371823+AdamJC-Git@users.noreply.github.com>
2026-09-23 09:17:20 +0100

    Update SecretsController.cs

--- Controllers/SecretsController.cs
+++ Controllers/SecretsController.cs
@@ -11,7 +11,7 @@ namespace ContainerApp_WebApi_Secrets.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Test Get Succeeded 1";
+            return "Test Get Succeeded 1 - v1";
         }
         [HttpGet("test")]
         public string TestGet()
